9061e7b0a9efc55a16e4e4f8346a61c08308d5e2
glucas038 <lucas-gabriel70@live.com>
2023-02-20 05:40:25 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello world! " + (a+b));*/
+
+        Gato gato1 = new Gato();
+
+        System.out.println(gato1);
+    }
+}
